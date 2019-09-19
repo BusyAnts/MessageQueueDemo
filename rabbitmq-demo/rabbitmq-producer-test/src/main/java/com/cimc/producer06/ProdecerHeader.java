6c9c5264481a004cfbec298c86df4068eae7af1e
BusyAnts <chenz16888@163.com>
2019-09-19 14:49:15 +0800

    RabbitMQ Header模式

--- rabbitmq-demo/rabbitmq-producer-test/src/main/java/com/cimc/producer06/ProdecerHeader.java
+++ rabbitmq-demo/rabbitmq-producer-test/src/main/java/com/cimc/producer06/ProdecerHeader.java
@@ -1,20 +1,18 @@
-package com.cimc.producer05;
+package com.cimc.producer06;
 
-import com.rabbitmq.client.BuiltinExchangeType;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 /**
- * 通配符模式，生产者
- *
  * @author chenz
- * @create 2019-09-19 13:28
+ * @create 2019-09-19 14:07
  */
-public class ProducerTopic {
+public class ProdecerHeader {
     /**
      * 声明两个队列绑定到交换机
      */
@@ -22,9 +20,9 @@ public class ProducerTopic {
     private static final String QUEUE_INFO_SMS = "queue_info_sms";
 
     /**
-     * 声明交换机名称，类型为topic
+     * 声明交换机名称，类型为headers
      */
-    private static final String EXCHANGE_TOPIC_INFO = "exchange_topic_info";
+    private static final String EXCHANGE_HEADERS_INFO = "exchange_headers_info";
 
     public static void main(String[] args) throws IOException, TimeoutException {
         Connection connection = null;
@@ -48,7 +46,7 @@ public class ProducerTopic {
              * param1:交换机名称
              * param2:交换机类型 fanout、topic、direct、headers
              */
-            channel.exchangeDeclare(EXCHANGE_TOPIC_INFO, BuiltinExchangeType.TOPIC);
+            channel.exchangeDeclare(EXCHANGE_HEADERS_INFO, BuiltinExchangeType.HEADERS);
 
 
             /**
@@ -62,31 +60,38 @@ public class ProducerTopic {
             channel.queueDeclare(QUEUE_INFO_EMAIL, true, false, false, null);
             channel.queueDeclare(QUEUE_INFO_SMS, true, false, false, null);
 
+            Map<String, Object> headers_email = new Hashtable<String, Object>();
+            headers_email.put("info_type", "email");
+            Map<String, Object> headers_sms = new Hashtable<String, Object>();
+            headers_sms.put("info_type", "sms");
+
+            /**
+             * 交换机和队列绑定
+             * param1:队列名称
+             * param2:交换机名称
+             * param3:路由key
+             */
+            channel.queueBind(QUEUE_INFO_EMAIL, EXCHANGE_HEADERS_INFO, "", headers_email);
+            channel.queueBind(QUEUE_INFO_SMS, EXCHANGE_HEADERS_INFO, "", headers_sms);
+
             //发送邮件消息
             for (int i = 0; i < 10; i++) {
                 String message = "email info to user" + i;
-                /**
-                 * 消息发布方法
-                 * param1:Exchange的名称，如果没有指定，则使用Default Exchange,每个队列也会绑定那个默认的交换机
-                 *     但是不能显示绑定或解除绑定
-                 *     默认的交换机，routingKey等于队列名称
-                 * param2:routingKey,消息的路由Key，是用于Exchange（交换机）将消息转发到指定的消息队列
-                 * param3:消息包含的属性
-                 * param4:消息体
-                 */
-                channel.basicPublish(EXCHANGE_TOPIC_INFO, "info.email", null, message.getBytes());
+                Map<String,Object> headers = new HashMap<>();
+                headers.put("info_type","email");
+                AMQP.BasicProperties.Builder properties = new AMQP.BasicProperties.Builder();
+                properties.headers(headers);
+                channel.basicPublish(EXCHANGE_HEADERS_INFO, "", properties.build(), message.getBytes());
                 System.out.println("Send Message is:'" + message + "'");
             }
             //发送短信消息
             for (int i = 0; i < 8; i++) {
                 String message = "sms info to user" + i;
-                channel.basicPublish(EXCHANGE_TOPIC_INFO, "info.sms", null, message.getBytes());
-                System.out.println("Send Message is:'" + message + "'");
-            }
-            //同时发送短信和邮件消息
-            for (int i = 0; i < 6; i++) {
-                String message = "sms and email info to user" + i;
-                channel.basicPublish(EXCHANGE_TOPIC_INFO, "info.email.sms", null, message.getBytes());
+                Map<String,Object> headers = new HashMap<>();
+                headers.put("info_type","sms");
+                AMQP.BasicProperties.Builder properties = new AMQP.BasicProperties.Builder();
+                properties.headers(headers);
+                channel.basicPublish(EXCHANGE_HEADERS_INFO, "", properties.build(), message.getBytes());
                 System.out.println("Send Message is:'" + message + "'");
             }
         } catch (Exception ex) {
@@ -100,5 +105,4 @@ public class ProducerTopic {
             }
         }
     }
-
 }
